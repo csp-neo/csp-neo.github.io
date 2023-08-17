e3c349b8ca0a436e8bc1b1fc6571f365c9bd0642
csp-neo <142539512+csp-neo@users.noreply.github.com>
2023-08-17 23:06:55 +0300

    Add files via upload

---
+++ precache-manifest.0e76175281ee59462cb0235512146750.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb24b14ca4017633363d",
+    "url": "/css/app.7594d4db.css"
+  },
+  {
+    "revision": "eb24b14ca4017633363d",
+    "url": "/js/app.ee09ab17.js"
+  },
+  {
+    "revision": "c01069ca7ccf7539e7a6",
+    "url": "/css/chunk-vendors.7e437fc9.css"
+  },
+  {
+    "revision": "c01069ca7ccf7539e7a6",
+    "url": "/js/chunk-vendors.c843aab7.js"
+  },
+  {
+    "revision": "d1fa2c43d2e1abe71155926c043b7dfe",
+    "url": "/fonts/SofiaSansExtraCondensed-VariableFont_wght.d1fa2c43.ttf"
+  },
+  {
+    "revision": "ee7729051eb2faa01d864c4eb0c282db",
+    "url": "/fonts/Pelagiad.ee772905.ttf"
+  },
+  {
+    "revision": "fb505e28b6d130f08fe8f070e0d6b1b8",
+    "url": "/fonts/CONSOLA.fb505e28.TTF"
+  },
+  {
+    "revision": "0127db5944cef9f92135d2971367e803",
+    "url": "/fonts/DelaGothicOne-Regular.0127db59.ttf"
+  },
+  {
+    "revision": "07623219053462e7bfbf0d74eeaf5f0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0e0ee87ef7149b3df3820c53be5a02e",
+    "url": "/styles/vscode.css"
+  }
+];
